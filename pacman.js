890a198ab2c0ecc190bc8ddecae90ff7d17bcf9d
KasthuriRaja023 <91175896+KasthuriRaja023@users.noreply.github.com>
2021-10-26 18:38:23 +0530

    Pac-Man Coding

---
+++ pacman.js
@@ -0,0 +1,53 @@
+// pos is the PacMan image position variable- it is set to 0 initially
+var pos = 0;
+//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around. 
+let pageWidth = window.innerWidth;
+//This array contains all the PacMan movement images
+const pacArray = 
+[
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+
+// this variable defines what direction should PacMan go into:
+// 0 = left to right
+// 1 = right to left (reverse)
+var direction = 0;
+
+// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+var focus = 0;
+
+// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
+function Run() 
+{
+  let img = document.getElementById('PacMan');
+  let imgWidth = img.width;
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
+  img.src = pacArray[direction][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + 'px';
+  } else {
+    pos += 20;
+    img.style.left = pos + 'px';
+  }
+}
+// TODO: Add a Javascript setInterval() method that will call the Run() function above every 200 milliseconds. 
+// Inside of the Run() function you will also have to add an extra argument "pageWidth", which is declared on line 4 when you call the checkPageBounds() function below. 
+setInterval(Run,200)
+// This function determines the direction of PacMan based on screen edge detection. 
+function checkPageBounds(direction, imgWidth, pos, pageWidth) 
+{
+  pageWidth = window.innerWidth;
+  // TODO: Complete this to reverse direction upon hitting screen edge
+  if (direction == 0 && pos + imgWidth > pageWidth)
+  {direction = 1;}
+  if (direction == 1 && pos < 0)
+  {direction = 0;}
+  //
+  return direction;
+}
+
+//Please do not change
+module.exports = checkPageBounds;
